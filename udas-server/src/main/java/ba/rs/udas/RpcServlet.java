d1983656f95b1bbfa60f513e323b5291b02c4c02
Ladislav92 <milunovic.ladislav@gmail.com>
2018-11-28 23:51:12 +0100

    Renamed udas_server to udas-server. Mavenized project.

--- udas-server/src/main/java/ba/rs/udas/RpcServlet.java
+++ udas-server/src/main/java/ba/rs/udas/RpcServlet.java
@@ -1,4 +1,12 @@
+package ba.rs.udas.database;
+
+import ba.rs.udas.database.model.database.ConnectionManager;
+import ba.rs.udas.database.model.database.DataAdapter;
+import ba.rs.udas.database.model.database.MySqlAdapter;
+import ba.rs.udas.database.model.member.Member;
+
 import java.io.*;
+import java.sql.SQLException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 //TODO import Dao, member, DBConnection etc...
@@ -31,19 +39,26 @@ public class RpcServlet extends HttpServlet{
         response.setContentType("text/html");
             
         //This whole connection manager thing is stupid - TODO in version two alongside with token login system
-        ConnectionManager.connect(username, password);
+        try {
+            ConnectionManager.connect(databaseUser, databasePassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-        JSONOBject responseAsJSON = toJSONObject(request.getReader());
+        //JSONOBject responseAsJSON = toJSONObject(request.getReader());
         
         //TODO get first key from json object 
         String command = "a"; // responseAsJSON.getKey();
         
-        //Cheap way of doing this, should be done with reflection or something...
         switch(command){
             
             case "addMember": // For example - name has to be same as in DAO
-                Member newMember = jsonToMemberObject(jsonResponse.getValue());
-                boolean succeed = sqlAdapter.addMember(newMember);
+                Member newMember = null; //TODO = jsonToMemberObject(jsonResponse.getValue());
+                try {
+                    boolean succeed = sqlAdapter.addMember(newMember);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
                 //buildResponse(command, succeed); //TODO
 
                 break;
@@ -65,22 +80,22 @@ public class RpcServlet extends HttpServlet{
      * @param reader
      * @return
      */
-    public JSONObject toJSONObject(BufferedReader reader){
-        StringBuffer jb = new StringBuffer();
-        String line = null;
-        try {
-        
-          while ((line = reader.readLine()) != null)
-            jb.append(line);
-        } catch (Exception e) { /*report an error*/ }
-      
-        try {
-          JSONObject jsonObject =  HTTP.toJSONObject(jb.toString());
-        } catch (JSONException e) {
-          // crash and burn
-          throw new IOException("Error parsing JSON request string");
-        }
-    }
+//    public JSONObject toJSONObject(BufferedReader reader){
+//        StringBuffer jb = new StringBuffer();
+//        String line = null;
+//        try {
+//
+//          while ((line = reader.readLine()) != null)
+//            jb.append(line);
+//        } catch (Exception e) { /*report an error*/ }
+//
+//        try {
+//          JSONObject jsonObject =  HTTP.toJSONObject(jb.toString());
+//        } catch (JSONException e) {
+//          // crash and burn
+//          throw new IOException("Error parsing JSON request string");
+//        }
+//    }
 
 }
 
