29d573600c1f55145e7265fc63e0ea0b18326412
Ladislav92 <milunovic.ladislav@gmail.com>
2018-11-29 21:43:53 +0100

    Brushing RpcServlet docs.

--- udas-server/src/main/java/ba/rs/udas/database/model/endpoint/RpcServlet.java
+++ udas-server/src/main/java/ba/rs/udas/database/model/endpoint/RpcServlet.java
@@ -1,4 +1,4 @@
-package ba.rs.udas.database.model;
+package ba.rs.udas.database.model.endpoint;
 
 import ba.rs.udas.database.model.dao.ConnectionManager;
 import ba.rs.udas.database.model.dao.DataAdapter;
@@ -9,16 +9,14 @@ import java.io.*;
 import java.sql.SQLException;
 import javax.servlet.*;
 import javax.servlet.http.*;
-//TODO import Dao, member, DBConnection etc...
-
 
 /**
  * In current implementation login is handled from client side, for productivity sake and its temporary.
  * Rpc Servlet communicates with post requests with UDAS dao client in JSON format:
  * 
  *  {key : {param1, param2}}
- * 
- * Where params can be valid json objects too or lists
+ *
+ * Where where key is method name (maybe even full path) and value is valid JSON object containing arguments to it.
  */
 public class RpcServlet extends HttpServlet{
 
@@ -49,7 +47,7 @@ public class RpcServlet extends HttpServlet{
         
         //TODO get first key from json object 
         String command = "a"; // responseAsJSON.getKey();
-        
+        //TODO at one point make use of reflection instead of switch-case
         switch(command){
             
             case "addMember": // For example - name has to be same as in DAO
