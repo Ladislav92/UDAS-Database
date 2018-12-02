d7013a9955e3c3bec5b593d87ded75289c7769d4
zeljic <zeljic@gmail.com>
2018-12-02 18:12:54 +0100

    rpc refactoring

---
+++ udas-server/src/main/java/ba/rs/udas/database/model/endpoint/RPCServlet.java
@@ -0,0 +1,52 @@
+package ba.rs.udas.database.model.endpoint;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class RPCServlet extends HttpServlet {
+
+	private static final Logger logger = LogManager.getLogger();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String c = req.getHeader("X-UDAS-class");
+		String m = req.getHeader("X-UDAS-method");
+
+		if (c == null || m == null) {
+			logger.error("Unable to get class and method from request");
+
+			resp.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
+
+			return;
+		}
+
+		try {
+			final Class<?> clazz = getClass().getClassLoader().loadClass(c);
+
+			final Method method = clazz.getMethod(m, HttpServletRequest.class, HttpServletResponse.class);
+
+			method.invoke(clazz.getDeclaredConstructor().newInstance(), req, resp);
+		} catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+			logger.error(e);
+
+			resp.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
+		}
+
+	}
+}
+
+
