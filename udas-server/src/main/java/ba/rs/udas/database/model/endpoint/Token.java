58ddee59527cf6e2f5a87ed559eadcdc5bbd1b52
zeljic <zeljic@gmail.com>
2018-12-08 09:57:48 +0100

    use sha-256 to generate token (WiP)

--- udas-server/src/main/java/ba/rs/udas/database/model/endpoint/Token.java
+++ udas-server/src/main/java/ba/rs/udas/database/model/endpoint/Token.java
@@ -1,12 +1,30 @@
 package ba.rs.udas.database.model.endpoint;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.UUID;
 
 class Token {
+	private static final Logger logger = LogManager.getLogger();
 	private String token;
 
 	Token() {
 		this.token = UUID.randomUUID().toString();
+
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+			byte[] buff = md.digest(this.token.getBytes(StandardCharsets.UTF_8));
+
+			this.token = Base64.getEncoder().encodeToString(buff);
+		} catch (NoSuchAlgorithmException e) {
+			logger.warn("Unable to use SHA-256 to generate token");
+		}
 	}
 
 	String getToken() {
